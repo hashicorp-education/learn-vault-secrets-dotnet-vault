ba8d64d6258f02ff9f1ce1a7d37fba376e310925
Ken Keller <ken.keller@hashicorp.com>
2026-06-13 19:12:39 -0500

    .net v8

--- database/import-data.sh
+++ database/import-data.sh
@@ -2,7 +2,7 @@
 sleep 15s
 
 # run the setup script to create the DB and the schema in the DB
-/opt/mssql-tools18/bin/sqlcmd -S localhost -U sa -P Testing!123 -d master -i setup.sql -No
+/opt/mssql-tools18/bin/sqlcmd -S localhost -U sa -P 'Testing!123' -d master -i setup.sql -No
 
 # upload projects.csv data
-/opt/mssql-tools18/bin/bcp Projects in /usr/src/app/projects.csv -S localhost -U sa -P Testing!123 -d HashiCorp -c -t ',' -u
+/opt/mssql-tools18/bin/bcp Projects in /usr/src/app/projects.csv -S localhost -U sa -P 'Testing!123' -d HashiCorp -c -t ',' -C -F2 -F2 -u
